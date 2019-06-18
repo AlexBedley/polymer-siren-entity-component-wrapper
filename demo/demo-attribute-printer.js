a638f4b001eca9a2105eba40c322c822d198100b
Alex Bedley <alex.bedley@D2L.com>
2019-06-18 13:08:50 -0400

    tabify

--- demo/demo-attribute-printer.js
+++ demo/demo-attribute-printer.js
@@ -1,27 +1,27 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 
 class DemoAttributePrinter extends PolymerElement {
-  static get is() {
-    return 'demo-attribute-printer';
-  }
+	static get is() {
+		return 'demo-attribute-printer';
+	}
 
-  static get template() {
-    return html`
-      <p>[[href]]</p>
-      <p>[[token]]</p>
-    `;
-  }
+	static get template() {
+		return html`
+			<p>[[href]]</p>
+			<p>[[token]]</p>
+		`;
+	}
 
-  static get properties() {
-    return {
-      href: {
-        type: String
-      },
-      token: {
-        type: String
-      }
-    };
-  }
+	static get properties() {
+		return {
+			href: {
+				type: String
+			},
+			token: {
+				type: String
+			}
+		};
+	}
 
 
 }
--- polymer-siren-entity-component-wrapper.js
+++ polymer-siren-entity-component-wrapper.js
@@ -6,45 +6,45 @@ const token = 'token';
 const tokenId = '#' + token;
 
 class PolymerSirenEntityComponentWrapper extends PolymerElement {
-  static get is() {
-    return 'polymer-siren-entity-component-wrapper';
-  }
+	static get is() {
+		return 'polymer-siren-entity-component-wrapper';
+	}
 
-  static get template() {
-    return html`
-      <label for=href>Href:</label>
-      <input id=href type="text" on-change="onChange"/>
-      <label for=token>Token:</label>
-      <input id=token type="text" on-change="onChange"/>
-      <slot></slot>
-    `;
-  }
+	static get template() {
+		return html`
+			<label for=href>Href:</label>
+			<input id=href type="text" on-change="onChange"/>
+			<label for=token>Token:</label>
+			<input id=token type="text" on-change="onChange"/>
+			<slot></slot>
+		`;
+	}
 
-  static get properties() {
-    return {};
-  }
+	static get properties() {
+		return {};
+	}
 
-  get child() {
-    return this.shadowRoot.querySelector('slot')
-      .assignedNodes()
-      .filter(node => node.nodeName !== '#text') // remove whitespace nodes
-      .shift();
-  }
+	get child() {
+		return this.shadowRoot.querySelector('slot')
+			.assignedNodes()
+			.filter(node => node.nodeName !== '#text') // remove whitespace nodes
+			.shift();
+	}
 
-  get href() {
-    return this.shadowRoot.querySelector(hrefId);
-  }
+	get href() {
+		return this.shadowRoot.querySelector(hrefId);
+	}
 
-  get token() {
-    return this.shadowRoot.querySelector(tokenId);
-  }
+	get token() {
+		return this.shadowRoot.querySelector(tokenId);
+	}
 
-  onChange(e) {
-    const child = this.child;
+	onChange(e) {
+		const child = this.child;
 
-    child.href = this.href.value;
-    child.token = this.token.value;
-  }
+		child.href = this.href.value;
+		child.token = this.token.value;
+	}
 }
 
 window.customElements.define(PolymerSirenEntityComponentWrapper.is, PolymerSirenEntityComponentWrapper);
